324071a9ea1b5799631e3ce1f835a8a4d87595b9
Eduard-2021 <sergsim1978@gmail.com>
2021-09-19 20:27:16 +0300

    Initial Commit

---
+++ VK_PlusSwiftUI/VK_PlusSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_PlusSwiftUIApp.swift
+//  VK_PlusSwiftUI
+//
+//  Created by Eduard on 19.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct VK_PlusSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
